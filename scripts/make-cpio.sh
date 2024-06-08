3822327a6a927cec716f6bf0f38b1d2a0805cd08
Easton Elliott <easton@geekness.eu>
2024-06-08 01:29:12 -0700

    Fix paths in cpio archive

--- scripts/make-cpio.sh
+++ scripts/make-cpio.sh
@@ -1,2 +1,5 @@
-find ./root -print0 | cpio --null -ov --format=newc | gzip -9 > rootfs.cpio.gz
-cp rootfs.cpio.gz ../bootable_image/boot
+#!/bin/bash
+cd root/
+find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../rootfs.cpio.gz
+# Optional:
+# cp rootfs.cpio.gz ../bootable_image/boot
